5841b154b21c4857295bb0e43fd09f55c0fd63f9
KimChhengChhim <114902030+KimChhengChhim@users.noreply.github.com>
2024-03-20 19:40:23 +0700

    In MessagesController
    change name to username in order to find data from username field on database

--- src/Http/Controllers/MessagesController.php
+++ src/Http/Controllers/MessagesController.php
@@ -332,7 +332,7 @@ class MessagesController extends Controller
         $getRecords = null;
         $input = trim(filter_var($request['input']));
         $records = User::where('id','!=',Auth::user()->id)
-                    ->where('name', 'LIKE', "%{$input}%")
+                    ->where('username', 'LIKE', "%{$input}%")
                     ->paginate($request->per_page ?? $this->perPage);
         foreach ($records->items() as $record) {
             $getRecords .= view('Chatify::layouts.listItem', [
